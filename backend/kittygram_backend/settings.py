acb44dd4d91ebf49965835805a35b81049b9ed0a
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-08-09 15:16:41 +0300

    ver 1.0

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = [
+    'localhost',
+    '89.169.177.177',
+    'thunderkitty.hopto.org'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
